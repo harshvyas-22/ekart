3491e9b0bb29b7c873676387abeae78aa7e70fb1
harsh <Harshvyas98397916@gmail.com>
2025-03-22 17:27:37 +0530

    solving

--- backend/server.js
+++ backend/server.js
@@ -1,34 +1,30 @@
 import express from "express";
 import dotenv from "dotenv";
-import { connectDB } from "./config/db.js";
-import productRoutes from "./routes/product.routes.js";
-import cors from "cors";
 import path from "path";
 
+import { connectDB } from "./config/db.js";
+
+import productRoutes from "./routes/product.route.js";
+
 dotenv.config();
+
 const app = express();
-const port = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
+
 const __dirname = path.resolve();
 
-app.use(
-  cors({
-    origin: "https://ekart-3.onrender.com/",
-    credentials: true,
-  })
-);
+app.use(express.json()); // allows us to accept JSON data in the req.body
 
-app.use(express.json());
-
-app.use("/products", productRoutes);
+app.use("/api/products", productRoutes);
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "/frontend/dist")));
-  app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"))
-  );
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
 }
 
-app.listen("https://ekart-3.onrender.com/", () => {
+app.listen(PORT, () => {
   connectDB();
-  console.log("Server is running on https://ekart-3.onrender.com/");
+  console.log("Server started at http://localhost:" + PORT);
 });
